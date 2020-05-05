6cd6bc0f5e78e715450c073029a00ac5ed436234
Michelle Kaplan <kaplan.michelle7@gmail.com>
2020-05-04 18:55:16 -0600

    Add map over local data for the newscontainer

--- src/components/NewsContainer/NewsContainer.js
+++ src/components/NewsContainer/NewsContainer.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import NewsArticle from '../NewsArticle/NewsArticle';
 import './NewsContainer.css'
 
-const NewsContainer = () => {
+const NewsContainer = ({local}) => {
 
-    
+    const localNews = local.map(data => {
+        return (
+            <NewsArticle
+                img={data.img}
+                headline={data.headline}
+                description={data.description}
+                url={data.url}
+                key={data.id}
+            />
+        )
+    })
+
+    return (
+        <section className='card-container'>
+            {localNews}
+        </section>
+    )
 }
 
 export default NewsContainer;
