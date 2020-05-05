139d68bdb1c18944a58954d3f7086ab9c9bb419c
Michelle Kaplan <kaplan.michelle7@gmail.com>
2020-05-04 23:11:39 -0600

    Add styling for nav bar

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -6,6 +6,11 @@ const Menu = () => {
     return (
         <nav className='nav'>
             <h1>What's <span>New?</span></h1>
+            <button className='nav-button'>Local News</button>
+            <button className='nav-button'>Technology</button>
+            <button className='nav-button'>Entertainment</button>
+            <button className='nav-button'>Science</button>
+            <button className='nav-button'>Health</button>
         </nav>
     )
 }
