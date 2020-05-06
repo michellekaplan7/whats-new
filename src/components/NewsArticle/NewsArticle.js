01084d60361176c0f7a79b82dbcd53e43efdf17e
Michelle Kaplan <kaplan.michelle7@gmail.com>
2020-05-06 15:47:41 -0600

    Add error image

--- src/components/NewsArticle/NewsArticle.js
+++ src/components/NewsArticle/NewsArticle.js
@@ -3,9 +3,13 @@ import './NewsArticle.css';
 import PropTypes from 'prop-types';
 
 const NewsArticle = ({ img, headline, description, url, id }) => {
+    const addErrorImage = (event) => {
+        event.target.src = require('../../image-not-found.png')
+    }
+
     return (
         <div className='card' id={id}>
-            <img className='card-image' src={img} alt={headline} />
+            <img className='card-image' src={img} alt={headline} onError={addErrorImage} />
             <div className='card-body'>
                 <h2>{headline}</h2>
                 <p>{description}</p>
