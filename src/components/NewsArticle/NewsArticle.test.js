2c2ae2c537c40c4c238df1cbe8991e97fad71c88
Michelle Kaplan <kaplan.michelle7@gmail.com>
2020-05-07 12:58:47 -0600

    Add test for NewsArticle component

---
+++ src/components/NewsArticle/NewsArticle.test.js
@@ -0,0 +1,22 @@
+import React from "react";
+import NewsArticle from "./NewsArticle";
+import { render } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+
+describe("NewsArticle", () => {
+  it("displays correct information on the NewsArticle card", () => {
+    const cardInfo = {
+      id: 1,
+      url: "url here",
+      description: "You thought you survived Coronavirus, only to get stung by a MURDER Hornet!?",
+      headline: "Murder Hornets Oh My!",
+      img: "goodbye",
+    };
+    const { getByText, getAllByRole, getByAltText } = render(<NewsArticle {...cardInfo} />);
+    expect(getByText("Read Article →")).toBeInTheDocument();
+    expect(getByText("You thought you survived Coronavirus, only to get stung by a MURDER Hornet!?")).toBeInTheDocument();
+    expect(getByText("Murder Hornets Oh My!")).toBeInTheDocument();
+    expect(getAllByRole("button")).toHaveLength(1)
+    expect(getByAltText('Murder Hornets Oh My!')).toBeInTheDocument();
+  });
+});
