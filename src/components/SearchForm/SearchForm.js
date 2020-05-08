74f93bb25e1981ceedd683e794ab8df7cdfe91ed
Michelle Kaplan <kaplan.michelle7@gmail.com>
2020-05-07 18:17:12 -0600

    Add readme

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import "./SearchForm.css";
 import PropTypes from "prop-types";
-import { render } from "@testing-library/react";
 
 class SearchForm extends Component {
   constructor() {
